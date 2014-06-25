913c1ac197266765dc63fed41f442dca37ac182b
square718 <square718@hotmail.com>
2014-06-25 09:57:53 +0800

    Revert "css fonts img js"
    This reverts commit 8582a0887fb913fa929579c0af64c4fffcd58915.

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,8 @@ var path = require('path'),
 	routes = {};
 
 routes.index = function(req, res){
-	//res.render('index', { title: 'Useful People1' });
-	res.sendfile(path.resolve('./files/practice.html'));
-	//res.sendfile(path.resolve('./files/test_client.html'));
+  res.render('index', { title: 'Useful People' });
 };
-
-routes.bootstrap = function(req, res){
-	//res.render('index', { title: 'Useful People1' });
-	res.sendfile(path.resolve('./files/index.html'));
-	//res.sendfile(path.resolve('./files/test_client.html'));
-};
-routes.bootstrap2 = function(req, res){
-	//res.render('index', { title: 'Useful People1' });
-	res.sendfile(path.resolve('./files/index2.html'));
-	//res.sendfile(path.resolve('./files/test_client.html'));
-};
-
 
 routes.io_test = function(req, res){
 	res.sendfile(path.resolve('./files/test_client.html'));
